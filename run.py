a3d0043db3db7e92034ffdad938cfaf942f8186c
Limpapat Bussaban <lim.bussaban@gmail.com>
2023-11-07 17:47:00 +0700

    add run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/aon', methods=['GET'])
+def aon():
+    username = request.headers.get('username', None)
+    usertoken = request.headers.get('token', None)
+    firstname = request.args.get('firstname', default=None)
+    lastname = request.args.get('lastname', default=None)
+    age = request.args.get('age', default=0)
+    if username != "Limpapat" or usertoken != "1234":
+        return jsonify({"message" : "Fuck you"}), 401
+    info = {
+        "name" : f"{firstname} {lastname}",
+        "age" : age
+    }
+    return jsonify(info)
+
+@app.route('/registered', methods=['POST'])
+def registered():
+    # username = request.headers.get('username', None)
+    # usertoken = request.headers.get('token', None)
+    input_data = request.get_json()
+    # alg....
+    return jsonify(input_data)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
